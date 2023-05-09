170ffe2b477868ff4b2884d2fb8dc1e9d653629c
elize21 <106829163+elize21@users.noreply.github.com>
2023-05-09 08:26:01 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const sessionName = "master1";
-const donet = "https://github.com/elize21/MASTER-AI";
-const owner = ["254701118127"]; // This will send a notification once the bot reconnects
+const sessionName = "dreaded1";
+const donet = "https://github.com/Fortunatusmokaya/DREADED-AI";
+const owner = ["254114018035"]; // This will send a notification once the bot reconnects
 const {
-  default: masterConnect,
+  default: dreadedConnect,
   useMultiFileAuthState,
   DisconnectReason,
   fetchLatestBaileysVersion,
@@ -139,7 +139,7 @@ async function startHisoka() {
   console.log(`using WA v${version.join(".")}, isLatest: ${isLatest}`);
   console.log(
     color(
-      figlet.textSync("MASTER-AI", {
+      figlet.textSync("DREADED-AI", {
         font: "Standard",
         horizontalLayout: "default",
         vertivalLayout: "default",
@@ -152,7 +152,7 @@ async function startHisoka() {
   const client = dreadedConnect({
     logger: pino({ level: "silent" }),
     printQRInTerminal: true,
-    browser: ["CHATGPT - MASTER", "Safari", "5.1.7"],
+    browser: ["CHATGPT - DREADED", "Safari", "5.1.7"],
     auth: state,
   });
 
@@ -278,10 +278,10 @@ async function startHisoka() {
         startHisoka();
       }
     } else if (connection === "open") {
-      console.log(color("Congrats, MASTER AI has successfully connected to this server", "green"));
-      console.log(color("Follow me on GitHub as elize21", "cyan"));
+      console.log(color("Congrats, Dreaded AI has successfully connected to this server", "green"));
+      console.log(color("Follow me on GitHub as Fortunatusmokaya", "red"));
       console.log(color("Text the bot number with !menu to check my command list"));
-      client.sendMessage(owner + "@s.whatsapp.net", { text: `Bot started!\n\nMaster OpenAi :)\n${donet}` });
+      client.sendMessage(owner + "@s.whatsapp.net", { text: `Bot started!\n\nDreaded OpenAi :)\n${donet}` });
     }
     // console.log('Connected...', update)
   });
